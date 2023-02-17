f1ce642e559fa579dbedbfe2ae7abe071ee14468
TAMA983472 <124493249+TAMA983472@users.noreply.github.com>
2023-02-17 14:29:56 +0900

    TODO追加機能だけ実装

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
 import "./styles.css";
+
+const LIST_CLASS = "list-low";
+/**
+ * TODO追加時の処理
+ * 入力ボックスを空にする。
+ * 未完了のTODOに要素を追加。
+ */
+const onClickAdd = () => {
+  //テキストボックスの値を取得して空にする。
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //要素を生成
+  const li = document.createElement("li");
+
+  const div = document.createElement("div");
+  div.className = LIST_CLASS;
+
+  const p = document.createElement("p");
+  p.textContent = inputText;
+
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    onClickComplete();
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    onClickDelete();
+  });
+
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  li.appendChild(div);
+
+  console.log(li);
+  //未完了のTODOに要素を追加
+  document.getElementById("imcomplete-list").appendChild(li);
+
+  console.log(document.getElementById("imcomplete-list"));
+};
+
+const onClickComplete = () => {
+  alert("完了したよ");
+};
+
+const onClickDelete = () => {
+  alert("削除したよ");
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
